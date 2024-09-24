a286657624fec4f71624cf1ed1949a00c0f99d25
blcthms <blcthms@Ubuntux.myguest.virtualbox.org>
2024-09-24 10:36:11 -0500

    updated configs for new working run

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'eb7136e832ab10996931d16ef76aeba50e444ed6'
+AESD_ASSIGNMENTS_VERSION = 'b4c984fe98cde2fd95ad632a637b304362f742b0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,13 +20,12 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0777 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0777 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0777 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	
-	$(INSTALL) -m 0777 $(@D)/finder-app/finder.sh $(TARGET_DIR)/root
-	$(INSTALL) -m 0777 $(@D)/finder-app/writer $(TARGET_DIR)/root
-	$(INSTALL) -m 0777 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/root
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
