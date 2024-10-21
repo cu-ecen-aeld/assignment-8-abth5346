3a013f082c39036412da5f7352b4611e3bfcd167
blcthms <blcthms@Ubuntux.myguest.virtualbox.org>
2024-10-20 22:10:24 -0500

    updated to fix bug in config file and added overlay permission

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 'cb92c16b72e1f3b3400e706032331796f5deba03'
+LDD_VERSION = '9b1de74050fd68587532d6ddb0f69cc9b80cf842'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,15 +14,14 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-abth5346.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS += misc-modules
-LDD_MODULE_SUBDIRS += scull
-
+LDD_MODULE_SUBDIRS += scull misc-modules
 
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/scull/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -d 0755 $(@D)/misc-modules/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load           $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload         $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 endef
-
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
